34c034d4197b95c61ec60a10866153da67a5c411
Saurabh Ajmera <sajmera@netflix.com>
2019-08-31 19:52:57 -0700

    importing external css in scss file

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./components/app/App";
 
 
